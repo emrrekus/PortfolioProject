0eb0d5d76368178fa3d2f78b2b996a50bf6543e1
Emre <112270755+emrrekus@users.noreply.github.com>
2024-10-04 18:33:54 +0300

    Hakkımda,eğitim deneyim kısımları düzeltildi veri girişi yapılıp partialview haline getirildi

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PortfolioProject.Models;
 
 namespace PortfolioProject.Controllers
 {
     public class DefaultController : Controller
     {
-        // GET: Default
+       DbMyPortfolioEntities db=new DbMyPortfolioEntities();
         public ActionResult Index()
         {
             return View();
@@ -28,5 +29,45 @@ namespace PortfolioProject.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialHeader()        {
+           
+
+            ViewBag.title= db.Profile.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description=db.Profile.Select(x=>x.Description).FirstOrDefault();
+            ViewBag.adress = db.Profile.Select(x => x.Adress).FirstOrDefault();
+            ViewBag.email = db.Profile.Select(x => x.Email).FirstOrDefault();
+            ViewBag.phone = db.Profile.Select(x => x.Phone).FirstOrDefault();
+            ViewBag.github = db.Profile.Select(x => x.Github).FirstOrDefault();
+            ViewBag.imageurl = db.Profile.Select(x => x.ImageUrl).FirstOrDefault();
+
+            return PartialView();
+        }
+
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.About.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialEducation()
+        {
+            var values = db.Education.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialExperience()
+        {
+            var values = db.Experience.ToList();
+            return PartialView(values);
+        }
+
+        public PartialViewResult PartialSkill()
+        {
+            var values = db.Skill.Where(x=>x.Status==true).ToList();    
+                
+            return PartialView(values);
+        }
+
+
     }
 }
